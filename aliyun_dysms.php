7e45fc18f943c9fb81b4f50b948b33d6b17b79fe
mangdin <41657311+mangdin@users.noreply.github.com>
2018-07-25 20:17:22 +0800

    阿里云 AccessKeyId和AccessKeySecret

---
+++ aliyun_dysms.php
@@ -0,0 +1,5 @@
+return  [
+    //阿里云 AccessKeyId和AccessKeySecret 获取地址：https://ak-console.aliyun.com/?spm=a2c4g.11186623.2.5.sMgjwG#/accesskey
+    'Aliyun_dysms_accessKeyId' =>   '',     //阿里云 AccessKeyId
+    'Aliyun_dysms_accessKeySecret'  =>  ''    //阿里云 AccessKeySecret
+];
