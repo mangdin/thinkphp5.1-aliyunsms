21218e515ff180f9ab4d993b1538718f84acf007
mangdin <41657311+mangdin@users.noreply.github.com>
2018-07-27 16:40:26 +0800

    修改配置文件信息

--- aliyun_dysms.php
+++ aliyun_dysms.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * Created by 合肥芒丁数据系统有限责任公司 www.mangdin.com.
+ * User: Administrator
+ * Date: 2018/7/27
+ * Time: 10:27
+ */
 return  [
     //阿里云 AccessKeyId和AccessKeySecret 获取地址：https://ak-console.aliyun.com/?spm=a2c4g.11186623.2.5.sMgjwG#/accesskey
     'Aliyun_dysms_accessKeyId' =>   '',     //阿里云 AccessKeyId
